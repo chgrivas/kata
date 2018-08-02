d16f6e0805997022f79fb267377bead92772f819
Chris Grivas <chgrivas7@gmail.com>
2018-08-02 22:39:22 +0300

    Add implementation for bowling kata

--- src/main/java/com/chgrivas/kata/playground/BowlingGame.java
+++ src/main/java/com/chgrivas/kata/playground/BowlingGame.java
@@ -2,19 +2,31 @@ package com.chgrivas.kata.playground;
 
 public class BowlingGame {
 
-  private int score;
+  private int[] rolledPinsArray;
 
   public BowlingGame() {
-    this.score = 0;
+    this.rolledPinsArray = new int[21];
   }
 
   public void roll(int...rolledPinsArray) {
-    for (int rolledPins : rolledPinsArray) {
-      score += rolledPins;
-    }
+    this.rolledPinsArray = rolledPinsArray;
   }
 
   public int calculateScore() {
+    int score = 0;
+    int frameIndex = 0;
+    for (int frame = 0; frame < 10; frame++) {
+      if (rolledPinsArray[frameIndex] == 10) {
+        score = score + 10 + rolledPinsArray[frameIndex + 1] + rolledPinsArray[frameIndex + 2];
+        frameIndex++;
+      } else if (rolledPinsArray[frameIndex] + rolledPinsArray[frameIndex + 1] == 10) {
+        score = score + 10 + rolledPinsArray[frameIndex + 2];
+        frameIndex += 2;
+      } else {
+        score = score + rolledPinsArray[frameIndex] + rolledPinsArray[frameIndex + 1];
+        frameIndex += 2;
+      }
+    }
     return score;
   }
 }
